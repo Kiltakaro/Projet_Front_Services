61f2773280f821462d7061fb65d69e9ddd7212b6
Kiltakaro <guboot08@gmail.com>
2024-12-05 11:43:47 +0100

    accueil template css

---
+++ front/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3B82F6',
+        'secondary': '#64748B',
+      },
+    },
+  },
+  plugins: [],
+}
