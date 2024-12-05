fed2c7d37ea6312e31371075098d354458fb8d53
Katorrz <morandbrice@gmail.com>
2024-12-05 14:50:12 +0100

    Ajout page de connexion et d'inscription$

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent, AccueilComponent],
+  imports: [RouterOutlet,NavbarComponent, AccueilComponent,LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
