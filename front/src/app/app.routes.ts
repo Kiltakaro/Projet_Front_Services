907fbe4465d2a6b50b941d033cbdc295946e2aef
Kiltakaro <guboot08@gmail.com>
2024-12-05 16:46:14 +0100

    Added redirect to navbar

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AccueilComponent },
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'profil', component: ProfilComponent},
+    { path: 'contact-us', component: ContactComponent}
+
+];
--- front/src/app/navbar/navbar.component.ts
+++ front/src/app/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ RouterLink, RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
