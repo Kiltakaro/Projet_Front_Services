e39a30d4956577cca8b22e4e658bebb0475896b9
Katorrz <morandbrice@gmail.com>
2024-12-05 14:18:54 +0100

    Fix navbar invisible

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent, AccueilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
